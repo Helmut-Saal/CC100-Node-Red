8dd19187a82adc7c40382e6cb785659839e5a4d4
sarahsecci <sarahelisabeth.secci@wago.com>
2022-08-01 10:15:17 +0200

    Troubleshooting the digital outputs

--- DigOut.js
+++ DigOut.js
@@ -6,7 +6,96 @@ module.exports = function(RED) {
     const do4 = 8;
     const TRUE = true;
     const FALSE= false;
+    const waitingTime = 1;          // Waiting time (in ms) for a new attempt to write a digital output (if file is blocked)
+    const maxTime = 5000;           // Max time (in ms) for the writing process
     global.monitor = false;
+
+    // Delay if several outputs are to be written simultaneously
+    function delay(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+
+    // Write a Digital Output
+    function writeDO(node, msg, value, fs, digout, starttime) {
+        var currentTime = Date.now();
+        var DO;  
+        
+        // Check which digital output should be set to generate appropriate messages
+        switch (digout) {
+            case do1: 
+                DO = "DO 1";
+                break;
+            case do2:
+                DO = "DO 2";
+                break;
+            case do3: 
+                DO = "DO 3";
+                break;
+            case do4:
+                DO = "DO 4";
+                break;
+            default:
+                DO = "NOT EXISTING DO"
+                break;
+        }
+
+        if((currentTime - starttime) <= maxTime) {
+            if (monitor === false) {
+                monitor = true;
+                // Read the state of the Output's from file
+                fs.readFile(DigOutPID,function(err, data) {
+                    if (err) {
+                        node.status({fill:"red",shape:"ring",text:"Failed"});
+                        return console.log(err);
+                    } else {            
+                        console.log("Read " + DO + " on CC100 was successful.");
+                        console.log("Raw Data: " + data);
+                        value = Number(data);
+                        // Write the Digital Output (value) if needed
+                        console.log("Writing " + DO + " on CC100.");
+                        node.status({fill:"green",shape:"ring",text:"Writing"});
+                        if (msg.payload === true & ((value & digout) != digout)){   
+                            value = (value + digout);
+                            msg.payload = value;
+                        } else if (msg.payload === false & ((value & digout) === digout)) {   
+                            value = (value - digout);
+                            msg.payload = value;
+                        } else {
+                            msg.payload = value;  
+                        }             
+                    }
+                    // Write the Digital Output to file
+                    const fs = require('fs');
+                    fs.writeFile(DigOutPID, String(msg.payload), function(err) {
+                        if(err) {
+                            node.status({fill:"red",shape:"ring",text:"Failed"});
+                            return console.log(err);
+                        } else {
+                            console.log("Write " + DO + " on CC100 was successful.");
+                            node.status({fill:"green",shape:"ring",text:"OK"});
+                        }
+                    }); 
+                    if (msg.payload  & (value & digout)) { 
+                        msg.payload=TRUE
+                    } else {
+                        msg.payload=FALSE  
+                    }
+                    monitor = false;
+                    node.send(msg);  
+                });
+            } else {
+                console.log("Write " + DO + " on CC100 was not possible.");                 // Writing blocked - waiting to write DO on CC100
+                node.status({fill:"yellow",shape:"ring",text:"Blocked"}); 
+                delay(waitingTime).then(() => {
+                    writeDO(node, msg, value, fs, digout, starttime); 
+                });
+            }
+        } else {
+            console.log("Writing " + DO + " on CC100 took too long.");
+            node.status({fill:"red",shape:"ring",text:"Took too long"});
+        }
+    }
+
     // Set Digital Output 1
     function setDO1(config) {
         RED.nodes.createNode(this,config);
@@ -14,60 +103,8 @@ module.exports = function(RED) {
         var value;
         const fs = require('fs');
         node.on('input', function(msg) {
-            if (monitor === false)
-            {
-                monitor = true;
-                // Read the state of the Output's from file
-                fs.readFile(DigOutPID,function(err, data) {
-                    if (err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }
-                    else {
-                        console.log("Read DO 1 on CC100 was successful.");
-                        console.log("Raw Data: " + data);
-                        value = Number(data);
-                        // Write the Digital Output 1 (value) if needed
-                        if (msg.payload === true & ((value & do1) != do1))
-                        {   
-                        value = (value + do1);
-                            msg.payload = value;
-                        }else if (msg.payload === false & ((value & do1) === do1))
-                        {   
-                            value = (value - do1);
-                            msg.payload = value;
-                        }else
-                        {
-                        msg.payload = value;  
-                        }             
-                }
-                    // Write the Digital Output 1 to file
-                    const fs = require('fs');
-                    fs.writeFile(DigOutPID, String(msg.payload), function(err) {
-                    if(err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }else {
-                        console.log("Write DO 1 on CC100 was successful.");
-                        node.status({fill:"green",shape:"ring",text:"OK"});
-                        }
-                    }); 
-                    if (msg.payload  & (value & do1))
-                    { 
-                        msg.payload=TRUE
-                    }
-                    else
-                    {
-                        msg.payload=FALSE  
-                    }
-                    monitor = false;
-                    node.send(msg);  
-                });
-            }else
-            {
-                console.log("Write DO 1 on CC100 was not possible.");
-                node.status({fill:"yellow",shape:"ring",text:"Blocked"});  
-            }
+            var starttime = Date.now();
+            writeDO(node, msg, value, fs, do1, starttime);
         });
     }
     RED.nodes.registerType("Set-DO1",setDO1); 
@@ -76,64 +113,12 @@ module.exports = function(RED) {
      function setDO2(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        var parts;
+        //var parts;
         var value;
         const fs = require('fs');
         node.on('input', function(msg) {
-            if (monitor === false)
-            {
-                monitor = true;
-                // Read the state of the Output's from file
-                fs.readFile(DigOutPID,function(err, data) {
-                    if (err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }
-                    else {
-                        console.log("Read DO 2 on CC100 was successful.");
-                        console.log("Raw Data: " + data);
-                        value = Number(data);
-                        // Write the Digital Output 2 (value) if needed
-                        if (msg.payload === true & ((value & do2) != do2))
-                        {   
-                        value = (value + do2);
-                            msg.payload = value;
-                        }else if (msg.payload === false & ((value & do2) === do2))
-                        {   
-                            value = (value - do2);
-                            msg.payload = value;
-                        }else
-                        {
-                        msg.payload = value;  
-                        }             
-                }
-                    // Write the Digital Output 2 to file
-                    const fs = require('fs');
-                    fs.writeFile(DigOutPID, String(msg.payload), function(err) {
-                    if(err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }else {
-                        console.log("Write DO 2 on CC100 was successful.");
-                        node.status({fill:"green",shape:"ring",text:"OK"});
-                        }
-                    });
-                    if (msg.payload  & (value & do2))
-                    { 
-                        msg.payload=TRUE
-                    }
-                    else
-                    {
-                        msg.payload=FALSE  
-                    }
-                    monitor = false; 
-                    node.send(msg);  
-                });
-            }else
-            {
-                console.log("Write DO 2 on CC100 was not possible.");
-                node.status({fill:"yellow",shape:"ring",text:"Blocked"});  
-            }
+            var starttime = Date.now();
+            writeDO(node, msg, value, fs, do2, starttime);
         });
     }
     RED.nodes.registerType("Set-DO2",setDO2); 
@@ -142,64 +127,12 @@ module.exports = function(RED) {
      function setDO3(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        var parts;
+        //var parts;
         var value;
         const fs = require('fs');
         node.on('input', function(msg) {
-            if (monitor === false)
-            {
-                monitor = true;
-                // Read the state of the Output's from file
-                fs.readFile(DigOutPID,function(err, data) {
-                    if (err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }
-                    else {
-                        console.log("Read DO 3 on CC100 was successful.");
-                        console.log("Raw Data: " + data);
-                        value = Number(data);
-                        // Write the Digital Output 3 (value) if needed
-                        if (msg.payload === true & ((value & do3) != do3))
-                        {   
-                        value = (value + do3);
-                            msg.payload = value;
-                        }else if (msg.payload === false & ((value & do3) === do3))
-                        {   
-                            value = (value - do3);
-                            msg.payload = value;
-                        }else
-                        {
-                        msg.payload = value;  
-                        }             
-                }
-                    // Write the Digital Output 3 to file
-                    const fs = require('fs');
-                    fs.writeFile(DigOutPID, String(msg.payload), function(err) {
-                    if(err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }else {
-                        console.log("Write DO 3 on CC100 was successful.");
-                        node.status({fill:"green",shape:"ring",text:"OK"});
-                        }
-                    });
-                    if (msg.payload  & (value & do3))
-                    { 
-                        msg.payload=TRUE
-                    }
-                    else
-                    {
-                        msg.payload=FALSE  
-                    }
-                    monitor = false; 
-                    node.send(msg);  
-                });
-            }else
-            {
-                console.log("Write DO 3 on CC100 was not possible.");
-                node.status({fill:"yellow",shape:"ring",text:"Blocked"});  
-            }
+            var starttime = Date.now();
+            writeDO(node, msg, value, fs, do3, starttime);
         });
     }
     RED.nodes.registerType("Set-DO3",setDO3); 
@@ -208,64 +141,12 @@ module.exports = function(RED) {
     function setDO4(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        var parts;
-        var value;
-        const fs = require('fs');
+        //var parts;
+        var value;   
+        const fs = require('fs');   
         node.on('input', function(msg) {
-            if (monitor === false)
-            {
-                monitor = true;
-                // Read the state of the Output's from file
-                fs.readFile(DigOutPID,function(err, data) {
-                    if (err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }
-                    else {
-                        console.log("Read DO 4 on CC100 was successful.");
-                        console.log("Raw Data: " + data);
-                        value = Number(data);
-                        // Write the Digital Output 4 (value) if needed
-                        if (msg.payload === true & ((value & do4) != do4))
-                        {   
-                        value = (value + do4);
-                            msg.payload = value;
-                        }else if (msg.payload === false & ((value & do4) === do4))
-                        {   
-                            value = (value - do4);
-                            msg.payload = value;
-                        }else
-                        {
-                        msg.payload = value;  
-                        }             
-                }
-                    // Write the Digital Output 4 to file
-                    const fs = require('fs');
-                    fs.writeFile(DigOutPID, String(msg.payload), function(err) {
-                    if(err) {
-                        node.status({fill:"red",shape:"ring",text:"Failed"});
-                        return console.log(err);
-                    }else {
-                        console.log("Write DO 4 on CC100 was successful.");
-                        node.status({fill:"green",shape:"ring",text:"OK"});
-                        }
-                    });
-                    if (msg.payload  & (value & do4))
-                    { 
-                        msg.payload=TRUE
-                    }
-                    else
-                    {
-                        msg.payload=FALSE  
-                    }
-                    monitor = false; 
-                    node.send(msg);  
-                });
-            }else
-            {
-                console.log("Write DO 4 on CC100 was not possible.");
-                node.status({fill:"yellow",shape:"ring",text:"Blocked"});  
-            }
+            var starttime = Date.now();
+            writeDO(node, msg, value, fs, do4, starttime);
         });
     }
     RED.nodes.registerType("Set-DO4",setDO4); 
@@ -281,8 +162,7 @@ module.exports = function(RED) {
                 if (err) {
                     node.status({fill:"red",shape:"ring",text:"Failed"});
                     return console.log(err);
-                }
-                else {
+                } else {
                     console.log("Read all DO's on CC100 was successful.");
                     console.log("Raw Data: " + data.toString());
                     node.status({fill:"green",shape:"ring",text:"OK"});
